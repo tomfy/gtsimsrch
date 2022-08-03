3221a4810288c6a04967b374d8cdc138c26ed179
tomfy <tly2@cornell.edu>
2022-08-03 15:52:31 -0400

    Fix bug causing male parent to not be uniquified.

--- src/uniquify_pedigree_file.pl
+++ src/uniquify_pedigree_file.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl -w
 use strict;
 
+# read in a cluster file (output of ...) and a
+# pedigree file (tab separated, accession, Fparent, Mparent ids in last 3 cols of each line)
+# output to stdout a pedigree file with the parental ids replaced with the representative of the cluster
+
 my $cluster_file = shift;
 my $pedigree_file = shift;
 
@@ -14,6 +18,7 @@ while(my $line = <$fhin>){
     my $the_repid = $cols[4];
     $repids{$the_repid} = 1;
     for my $id (@cols[4..$#cols]){
+     # print STDERR "$id $the_repid\n";
 	$clusterids_repid{$id} = $the_repid;
     }
 }
@@ -24,10 +29,15 @@ open $fhin, "<", "$pedigree_file";
 my $line1 = <$fhin>;
 print $line1;
 while(my $line = <$fhin>){
-    chomp $line;
+  $line =~ s/\s+$//; # remove newline, any other whitespace at end.
     my @cols = split("\t", $line);
     # replace the last 2 elements of @cols with cluster representatives, if belong to cluster
- #   print STDERR $cols[-1], "  ", $cols[-2], "  ";
+    #   print STDERR $cols[-1], "  ", $cols[-2], "  ";
+    # my $mparent = $cols[-1];
+    # my $fparent = $cols[-2];
+    # if($mparent =~ /\s/  or  $fparent =~ /\s/){
+    #   print STDERR "[$fparent] [$mparent] \n";
+    # }
     $cols[-1] = $clusterids_repid{$cols[-1]} // $cols[-1];
     $cols[-2] = $clusterids_repid{$cols[-2]} // $cols[-2];
  #   print STDERR $cols[-1], "\t", $cols[-2], "\n";
