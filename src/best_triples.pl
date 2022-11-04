0a71e89fa6057f416b2b8122e539e5b3eda1a6bb
tomfy <tly2@cornell.edu>
2022-11-04 13:29:21 -0400

    output parent ids as well as numerical value (z, d, ...) for each solution.

--- src/best_triples.pl
+++ src/best_triples.pl
@@ -10,16 +10,25 @@ while (my $line = <>) {
   my @cols = split(" ", $line);
   my $accid = $cols[0];
   my $parents = $cols[1] . " " . $cols[2];
+  my $cross_type = ($cols[1] eq $cols[2])? "self" : "bip";
   my $value = $cols[$the_col];
+  my $vc = "$value  $cross_type  $parents";
   if (exists $accid_solns{$accid}) {
-    push @{$accid_solns{$accid}}, $value; # "$parents  $value"; 
+    push @{$accid_solns{$accid}}, $vc; # "$parents  $value"; 
   } else {
-    $accid_solns{$accid} = [$value];
+    $accid_solns{$accid} = [$vc];
   }
 }
 
 for my $anid (keys %accid_solns) {
   my $solutions = $accid_solns{$anid};
-  my @sorted_solns = sort {$a <=> $b} @$solutions;
+  my @sorted_solns = sort {val($a) <=> val($b)} @$solutions;
   print "$anid  ", join("  ", @sorted_solns), "\n";
+}
+
+
+sub val{
+  my $vc = shift;
+  my ($value, $ct, $ps) = split(" ", $vc);
+  return $value;
 }
