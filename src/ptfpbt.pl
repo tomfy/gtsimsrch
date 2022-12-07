22166a8a2e5d321cfa17e70114516b598858fca8
tomfy <tly2@cornell.edu>
2022-12-06 22:13:23 -0500

    works now to compare pedigree_test and find_parents/best_triples output.

--- src/ptfpbt.pl
+++ src/ptfpbt.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
+use Scalar::Util qw(looks_like_number);
 
 my $ptfile = shift;
 my $btfile = shift;
@@ -10,31 +11,47 @@ open my $fhbt, "<", "$btfile";
 
 my %accid_ptline = ();
 my %accid_ptppd = ();
-while(my $line = <$fhpt>){
+# store pedigree_test information 
+while (my $line = <$fhpt>) {
   my @cols = split(" ", $line);
   my $acc = $cols[0];
   my $D = $cols[17];
-  if($D <= $maxD){
+  if (looks_like_number($D)  and  $D <= $maxD) {
     $accid_ptline{$acc} = $line;
     my ($p1, $p2) = @cols[2,3];
-    my $ppd = order_pair($p1, $p2) . " $D";
+    my $ppd = order_pair($p1, $p2) . " $D"; # Fparent id, Mparent id, D
     $accid_ptppd{$acc} = $ppd;
   }
 
 }
 
-while(my $line = <$fhbt>){
+# store find_parents/best_triples information
+while (my $line = <$fhbt>) {
   my @cols = split(" ", $line);
   my $accid = $cols[0];
   my $ptppd = $accid_ptppd{$accid} // undef;
-  if(defined $ptppd){
-  for(my ($offset, $i) = (2, 0); $offset+17 < scalar @cols; $offset += 23, $i++){
-    my $ppd = get_p1p2D(\@cols, $offset);
-    my $best_ppd = $ppd if($offset == 2);
+  #my ($ptp1, $ptp2, $ptD) = split(" ", $ptppd);
+  my $best_ppd = "XXX";
+  my $next_best_ppd = "xxx";
+  my $pedigree_rank = -1;
+  if (defined $ptppd) { # if pedigree_test had result for this accession ...
+    for (my ($offset, $i) = (2, 0); $offset+17 < scalar @cols; $offset += 23, $i++) {
+      my $ppd = get_p1p2Dstd(\@cols, $offset);
+      print STDERR "***   $accid  $ppd \n";
+      if($i == 0){
+	$best_ppd = $ppd;
+      }elsif($i == 1){
+	$next_best_ppd = $ppd;
+      }
+      if($ptppd eq $ppd){
+	$pedigree_rank = $i;
+	last if($i >= 1);
+      }
+    }
+    print "$accid    $ptppd   $best_ppd   $next_best_ppd   $pedigree_rank \n"; 
+  } else {
+    print STDERR "No pedigree for accession: $accid \n";
   }
-}else{
-
-}
 
 }
 
