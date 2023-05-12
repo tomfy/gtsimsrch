ad6a0c8d200268ec1a2939beedbe63c8ecc5bbd5
tomfy <tly2@cornell.edu>
2023-05-11 22:40:02 -0400

    store genotypes in 1 string per accession, saves memory

--- src/vcf2gts.pl
+++ src/vcf2gts.pl
@@ -6,17 +6,15 @@ use File::Spec 'splitpath';
 use Devel::Size qw(size total_size);
 
 # vcf file should have one of :
+
+# genotype GT, e.g. 0/1  or  0|1
 # dosage DS,
-# genotype GT, e.g. 0/0/0/1  or  0|0|0|1
 # allele depth AD, e.g. 21,19
 # and if it has GP (genotype prob., e.g. 0.9,0.16,0.04) or GQ (genotype quality, e.g. 98) 
 # we can also reject entries (i.e. regard as missing data) if these are not good enough
 
-# usage:  vcf2dosage.pl -i <input vcf file>  -o <output file>
+# usage:  vcf2gts.pl -i <input vcf file>  -o <output file>
 
-
-my $transpose = 1; # default is to transpose; use -notrans to output untransposed.
-# (duplicatesearch, find_parents require transposed)
 # vcf: columns correspond to accessions, rows to markers
 
 # if we don't believe can reliably resolve various heterozygous genotypes in polyploid case
@@ -24,16 +22,15 @@ my $transpose = 1; # default is to transpose; use -notrans to output untranspose
 my $map_to_012 = 0; # dosage = ploidy -> 2, 0 < dosage < ploidy -> 1, 0 -> 0, X -> X
 my $field_to_use = 'GT'; # default GT. If requested field is not present exit (or ???)
 # recognized choices are  GT (genotype e.g. '/1/0' ), DS (alternative allele dosage e.g. 2), AD (allele depths, e.g.'136:25' ).
-### could add another option: GP, i.e. choose whichever gt has est. greatest probability.
 my $ploidy = 2; # need to specify, will die if find dosages greater than specified ploidy.-
 my $inferred_ploidy = -1; # infer from data; die if > specified $ploidy
 my $delta = 0.1; # if not $map_to_012, round to integer if within +- $delta
                  # if map_to_012 [0, $delta ->0], [1-$delta, $ploidy-1+$delta] -> 1, [$ploidy-$delta, $ploidy] -> 2
-my $min_read_depth = 1;
+my $min_read_depth = 5;
 my $input_vcf_filename = undef;
 my $output_genotypes_filename = undef; # default: construct from input filename
 my $missing_data_string = 'X';
-my $minGQ = 96;			# if GQ present, must be >= this.
+my $minGQ = 0;	 # if GQ present, must be >= this.
 my $minGP = 0.0; # if GP present, there must be 1 genotype with prob >= $minGP; i.e. one genotype must be strongly preferred.
 my $min_marker_avg_pref_gt_prob = -1.0; # default is negative (meaning don't filter on this)
 my $max_marker_missing_data_fraction = 1.0; # remove markers with excessive missing data. Default is keep all.
@@ -44,7 +41,6 @@ my $plink_format = 0;
 GetOptions(
 	   'input_file|vcf=s' => \$input_vcf_filename,
 	   'output_file|dosage_file=s' => \$output_genotypes_filename,
-	   'transpose!' => \$transpose, # -notranspose to output untransposed. (duplicatesearch requires transposed which is default)
 	   'GQmin=f' => \$minGQ,	# min genotype quality.
 	   'GPmin=f' => \$minGP,	# 
 	   'field=s' => \$field_to_use,
@@ -71,7 +67,7 @@ if (!defined $output_genotypes_filename) { # construct an output filename from i
 }
 
 my $output_markerids_filename;
-if($plink_format){
+if ($plink_format) {
   $output_genotypes_filename =~ s/dosages$//;
   $output_genotypes_filename =~ s/[.]$//;
   $output_markerids_filename = $output_genotypes_filename . ".map";
@@ -86,7 +82,6 @@ open my $fhout, ">", "$output_genotypes_filename" or die "Couldn't open $output_
 # #####  output run parameters  #####
 
 $info_string .= "# output file: $output_genotypes_filename\n";
-$info_string .= "# transpose? " . (($transpose)? "Yes\n" : "No\n");
 $info_string .= "# data field to use: $field_to_use\n";
 $info_string .= "# delta: $delta ; min read depth: $min_read_depth\n";
 $info_string .= "# GPmin: $minGP ; GQmin: $minGQ\n";
@@ -122,29 +117,30 @@ print STDERR "# number of accession ids: #  ", scalar @col_ids, "\n";
 # #####  read the rows with genotype data  #####
 # print STDERR "plink map filename: $output_markerids_filename \n"; sleep(2);
 open my $fhmap, ">", "$output_markerids_filename" or die "Couldn't open $output_markerids_filename for writing.\n" if($plink_format);
-my @row_ids = (); # i.e. marker ids
+my @row_ids = ();		# i.e. marker ids
 my @alt_row_ids = ();
 my %rowid = ();
 my %altrowid = ();
 # my @rows = ();
-my @row_strings = ();
+#my @row_strings = ();
 my $missing_data_count = 0;
 my $markers_read_count = 0;
 my @accession_gt_strings = (); # ith element with be string of genotypes for the ith accession
 my %fieldused_count = (); # keys: 'DS', 'GT', etc.; values: count of gt data of that format.
+my $n_accessions = 0;
 while (<$fhin>) {
   my $marker_missing_data_count = 0;
   my $marker_alt_allele_count = 0;
   my @marker_dosage_distribution = ();
   my @genotypes_this_row = ();
-  my $row_gts_str = '';
+#  my $row_gts_str = '';
   my @cols = split(" ", $_);
   my $row_id = $cols[2]; # store row id in file (if these are not distinct, we will use $alt_row_id).
   my $alt_row_id = $cols[0] . "_" . $cols[1]; # construct a marker id from col[0] (chromosome number) and col[1] (position)
   my $chr_number = $cols[0];
   $chr_number =~ s/^chr//;
   my $plink_mapfile_str = "$chr_number\t$alt_row_id\t0\t" . $cols[1];
-#  print STDERR "$plink_mapfile_str\n";
+  #  print STDERR "$plink_mapfile_str\n";
   print $fhmap "$plink_mapfile_str\n" if($plink_format);
   my $format_str = $cols[8]; # e.g. 'GT:DS:AD' tells which types of data are present.
   my ($allele_zero, $allele_one) = @cols[3,4]; # e.g. 'A' and 'T' 
@@ -161,10 +157,11 @@ while (<$fhin>) {
   # (GT:DS:GP): 0|0:0.001:0.999,0.001,0    ((diploid)genotype:dosage:est_genotype_prob)
   @cols = @cols[9..$#cols];	# cols 9 and above have genotype info.
   # for my $e (@cols) {
-  while(my($acc_idx, $e) = each @cols){
+  $n_accessions = 0;
+  while (my($acc_idx, $e) = each @cols) {
     my $dosage = $missing_data_string; # indicates missing data
-    my $first_allele = undef; # 0: ref allele, 1: alt allele.
- #   my $plnk_gt = "0\t0\t"; # this is what plink wants
+    my $first_allele = undef;	       # 0: ref allele, 1: alt allele.
+    #   my $plnk_gt = "0\t0\t"; # this is what plink wants
     my @field_values = split(":", $e);
     die if(scalar @field_values != $nfields);
 
@@ -233,7 +230,7 @@ while (<$fhin>) {
       # $pgtprob_count++;
       $dosage = $missing_data_string if($preferred_gt_prob < $minGP);
     }
-    if($GQidx >= 0  and  $field_values[$GQidx] < $minGQ){
+    if ($GQidx >= 0  and  $field_values[$GQidx] < $minGQ) {
       $dosage = $missing_data_string;
     }
 
@@ -250,17 +247,14 @@ while (<$fhin>) {
 	($dosage == 0)? "$allele_zero\t$allele_zero\t" :
 	($dosage == 1)? (($first_allele eq '0')? "$allele_zero\t$allele_one\t" : "$allele_one\t$allele_zero\t") :
 	($dosage == 2)? "$allele_one\t$allele_one\t" : "0\t0\t";
-      push @genotypes_this_row, $plnk_gt;
       $accession_gt_strings[$acc_idx] .= $plnk_gt;
-    } else {	      # plink
-      #  print STDERR "first allele $first_allele \n";
-      push @genotypes_this_row, $dosage;
-      $row_gts_str .= "$dosage";
-       $accession_gt_strings[$acc_idx] .= "$dosage ";
+    } else {	      # not plink
+      $accession_gt_strings[$acc_idx] .= "$dosage ";
     }
+    $n_accessions++;
   }				# end loop over entries in a row
   die "Inferred ploidy ($inferred_ploidy) is greater than specified ploidy ($ploidy).\n" if($inferred_ploidy > $ploidy);
-  my $marker_total_allele_count = $ploidy*((scalar @genotypes_this_row) - $marker_missing_data_count);
+  my $marker_total_allele_count = $ploidy*($n_accessions - $marker_missing_data_count);
   my $marker_alt_allele_frequency = $marker_alt_allele_count/$marker_total_allele_count;
   my $marker_minor_allele_frequency = ($marker_alt_allele_frequency <= 0.5)?
     $marker_alt_allele_frequency :
@@ -268,29 +262,17 @@ while (<$fhin>) {
 
   $missing_data_count += $marker_missing_data_count;
 
-  die "# n col ids: ", scalar @col_ids, "; n dosages in row: ", scalar @genotypes_this_row, "\n" if(scalar @genotypes_this_row != scalar @col_ids);
-  #  print "# n col ids: ", scalar @col_ids, "; n dosages in row: ", scalar @genotypes_this_row, "\n";
-  #sleep(1);
- # $avg_pref_gt_prob /= $pgtprob_count;
-  my $marker_missing_data_fraction = $marker_missing_data_count/(scalar @genotypes_this_row);
-  
+  die "# n col ids: ", scalar @col_ids, "; n dosages in row: ", $n_accessions, "\n" if($n_accessions != scalar @col_ids);
+  my $marker_missing_data_fraction = $marker_missing_data_count/$n_accessions;
+
   #  print STDERR "# avg pref gt prob: $avg_pref_gt_prob ;  marker md fraction: $marker_missing_data_fraction  $marker_missing_data_count\n";
   if ($avg_pref_gt_prob >= $min_marker_avg_pref_gt_prob  and
       $marker_missing_data_fraction <= $max_marker_missing_data_fraction and
       $marker_minor_allele_frequency >= $min_marker_maf) { # there is good data for this marker; store and output later.
-  #  print STDERR "row size, total_size: ", size(\@genotypes_this_row), "  ", total_size(\@genotypes_this_row), "\n";
- #   print STDERR "row str size, total_size: ", size(\$row_gts_str), "  ", total_size(\$row_gts_str), "\n";
-   # push @rows, \@genotypes_this_row;
-    push @row_strings, $row_gts_str;
- #   print STDERR $row_gts_str, "\n";
- #   print STDERR "rows so far. size, total_size:", size(\@rows), "  ", total_size(\@rows), "\n";
     push @row_ids, $row_id;	# store row (marker) id
     $rowid{$row_id} = 1; # also store in a hash to see whether all ids are distinct.
     push @alt_row_ids, $alt_row_id;
     $altrowid{$alt_row_id} = 1;
-  #  my $n_elems_out =  scalar @row_strings  *  length $row_gts_str;
-    # print STDERR "### $n_elems_out  ";
-    # while (my($i, $c) = each @marker_dosage_distribution) {
     for my $i (0..$ploidy) {
       # my $c = $marker_dosage_distribution[$i];
       $dosage_distribution[$i] +=  $marker_dosage_distribution[$i] // 0;
@@ -306,7 +288,7 @@ while (<$fhin>) {
 close $fhin;
 close $fhmap if($plink_format);
 $info_string .= "# Done processing all $markers_read_count rows (markers).\n";
-$info_string .= "# ", scalar @row_strings . " rows stored to be output.\n";
+$info_string .= "# ", scalar @row_ids . " rows stored to be output.\n";
 $info_string .= "# Data format: count.  ";
 while (my($f, $c) = each %fieldused_count) {
   $info_string .= "$f: $c;  ";
@@ -330,7 +312,7 @@ for my $rid (@row_ids) {    # select the ids of the rows we are using.
   push @row_ids_out, $rid if(defined $rid);
 }
 
-my ($n_rows_out, $n_cols_out) = (scalar @row_strings, scalar @col_ids);
+my ($n_rows_out, $n_cols_out) = (scalar @row_ids_out, scalar @col_ids);
 my $n_elements = $n_rows_out * $n_cols_out;
 $info_string = '';
 $info_string .= sprintf("# ploidy appears to be: %d\n", $ploidy);
@@ -344,62 +326,22 @@ print STDERR $info_string;
 print $fhout $info_string if(! $plink_format);
 
 # #####  print the output dosages  #####
-if (! $transpose) {
-  print $fhout "MARKER ", join(" ", @col_ids), "\n";
-  die if(scalar @row_ids_out  !=  scalar @row_strings);
-  while (my ($i, $row_dosages) = each(@row_strings)) {
-    print $fhout $row_ids_out[$i], "  ", join(" ", @$row_dosages), "\n";
+# transposed
+if (! $plink_format) {
+  print $fhout "MARKER ", join(" ", @row_ids_out), "\n";
+}
+while (my ($i, $col_id) = each @col_ids) {
+  # print col i (gts of ith accession) as a row
+  if ($plink_format) {
+    print $fhout "$col_id\t$col_id\t";
+    print $fhout "0\t0\t0\t0\t"; # vcftools always puts these 4 zeroes - not sure why or whether necessary
+  } else {
+    print $fhout "$col_id ";
   }
-} else {			# transpose
-  if(! $plink_format){
-    print $fhout "MARKER ", join(" ", @row_ids_out), "\n";
-  }
-  while (my ($i, $col_id) = each @col_ids) {
-    # print col i as a row
-    if($plink_format){
-      print $fhout "$col_id\t$col_id\t";
-      print $fhout "0\t0\t0\t0\t"; # vcftools always has these 4 zeroes - not sure why or whether necessary
-    }else{
-      print $fhout "$col_id  ";
-    }
-    if(1 or $plink_format){
-      my $gtstr = $accession_gt_strings[$i];
-	$gtstr =~ s/\t\s*$//;
-	print $fhout "$gtstr\n";
-      
-      # my $s = '';
-      # while(my($j, $r) = each @row_strings){
-      # 	#print $fhout join('', @$r), "\n";
-      # 	$s .= sprintf("%s", substr($r, $i, 1)->[$i]);
-      # 	# print $fhout $r->[$i];
-      # }
-      # $s =~ s/\t\s*$//;
-      # print $fhout $s, "\n";
-    }else{
-      if(0){
-    while (my($j, $r) = each @row_strings) {
-      my $d = $r->[$i];
-      # at this point $d = 0, 1, 2, ... , ploidy or X (missing data)
-      if ($map_to_012) {
-	if ($d eq $missing_data_string) {
-	  # no change
-	} elsif ($d eq $ploidy) {
-	  $d = 2;
-	} elsif ($d > 0) {
-	  $d = 1;
-	}			# else $d = 0, no change
-      }
-      print $fhout "$d ";
-    }
-    print $fhout "\n";
-  }else{
-    while(my($imrkr, $r) = each @row_strings){
-      my $d = substr($r, $i, 1);
-      print $fhout "$d ";
-    }
-    print $fhout "\n";
-  }
-  }
+  if (1 or $plink_format) {
+    my $gtstr = $accession_gt_strings[$i];
+    $gtstr =~ s/\t\s*$//;
+    print $fhout "$gtstr\n";
   }
 }
 close $fhout;
