92c47fa800fa7bf9968915b79fa0941aa8b67f9a
tomfy <tly2@cornell.edu>
2023-06-13 14:10:09 -0400

    not tests stderr output also. Just expects 6 lines, one of which is 'Successfully completed check_genotypesset'

--- test_duplicatesearch/ds_test.pl
+++ test_duplicatesearch/ds_test.pl
@@ -10,17 +10,11 @@ GetOptions(
 	   'Reset!' => \$reset_refs,
 	   );
 
-if($reset_refs){
-  system "chmod 666 *.stdout";
-  system "chmod 666 *.dsout";
-}
-# sleep (10);
-
 # test duplicatesearch comparing its output
 # to known good output for several inputs and
 # sets of run parameters.
 
-my $n_failed_tests;
+my $failed_tests;
 my $n_failed = 0;
 # *************************************************************
 #  yam941 data, unshuffled, est_max_dist = 0.2
@@ -34,8 +28,8 @@ my $test_dsout_file = "y941_gp0.9_maf0.08_maxd0.2_md0.25_un.dsout_x";
 if($reset_refs){
   reset_refs($input_file, $ref_stdout_file, $ref_dsout_file, " -est_max_dist 0.2  -un ");
 }else{
-  $n_failed_tests = test($test_name, $input_file, $ref_stdout_file, $test_stdout_file, $ref_dsout_file, $test_dsout_file, " -est_max_dist 0.2  -un ");
-  $n_failed++ if($n_failed_tests>0);
+  $failed_tests = test($test_name, $input_file, $ref_stdout_file, $test_stdout_file, $ref_dsout_file, $test_dsout_file, " -est_max_dist 0.2  -un ");
+  $n_failed++ if($failed_tests);
 }
 
 
@@ -51,10 +45,10 @@ $test_dsout_file = "y941_gp0.9_maf0.08_maxd0.2_md0.25.dsout_x";
 if($reset_refs){
   reset_refs($input_file, $ref_stdout_file, $ref_dsout_file, " -est_max_dist 0.2 ");
 }else{
-  $n_failed_tests = test($test_name, $input_file, $ref_stdout_file, $test_stdout_file, $ref_dsout_file, $test_dsout_file, " -est_max_dist 0.2 ");
-    $n_failed++ if($n_failed_tests>0);
+  $failed_tests = test($test_name, $input_file, $ref_stdout_file, $test_stdout_file, $ref_dsout_file, $test_dsout_file, " -est_max_dist 0.2 ");
+    $n_failed++ if($failed_tests);
 
-  #print "Passed ", $n_tests-$n_failed_tests, " of $n_tests.\n";
+  #print "Passed ", $n_tests-$failed_test, " of $n_tests.\n";
 }
 
 #**************************************************************
@@ -72,10 +66,10 @@ $test_dsout_file = "cassava_gp0.9_maf0.08_maxd0.17_md0.25.dsout_x";
 if($reset_refs){
   reset_refs($input_file, $ref_stdout_file, $ref_dsout_file, " -est_max_dist 0.17 ");
 }else{
-  $n_failed_tests = test($test_name, $input_file, $ref_stdout_file, $test_stdout_file, $ref_dsout_file, $test_dsout_file, " -est_max_dist 0.17 ");
-    $n_failed++ if($n_failed_tests>0);
+  $failed_tests = test($test_name, $input_file, $ref_stdout_file, $test_stdout_file, $ref_dsout_file, $test_dsout_file, " -est_max_dist 0.17 ");
+    $n_failed++ if($failed_tests);
 
-  #print "Passed ", $n_tests-$n_failed_tests, " of $n_tests.\n";
+  #print "Passed ", $n_tests-$failed_test, " of $n_tests.\n";
 }
 
 #**************************************************************
@@ -93,14 +87,11 @@ $test_dsout_file = "cassava_gp0.9_maf0.08_md0.25.dsout_x";
 if($reset_refs){
   reset_refs($input_file, $ref_stdout_file, $ref_dsout_file, "");
 }else{
-  $n_failed_tests = test($test_name, $input_file, $ref_stdout_file, $test_stdout_file, $ref_dsout_file, $test_dsout_file, "");
-  $n_failed++ if($n_failed_tests>0);
+  $failed_tests = test($test_name, $input_file, $ref_stdout_file, $test_stdout_file, $ref_dsout_file, $test_dsout_file, "");
+  $n_failed++ if($failed_tests);
 
-  #print "Passed ", $n_tests-$n_failed_tests, " of $n_tests.\n";
+  #print "Passed ", $n_tests-$failed_test, " of $n_tests.\n";
 }
-
-  system "chmod 444 *.stdout";
-system "chmod 444 *.dsout";
 
 ##################################################################################
 
@@ -112,10 +103,8 @@ sub reset_refs{
 
   my $stderr_file = "stderr";
 
- # system "chmod 666 $ref_stdout_file";
-  # system "chmod 666 $ref_dsout_file";
-  #unlink $ref_stdout_file;
-  #unlink $ref_dsout_file;
+  system "chmod 666 $ref_stdout_file";
+  system "chmod 666 $ref_dsout_file";
 
   my $command = "duplicatesearch  -in $input_file  -out $ref_dsout_file  2> $stderr_file  > $ref_stdout_file ";
   $command .= " -maf 0.08  -accession_max 0.5  -seed 13572467  -t 4 ";
@@ -123,6 +112,9 @@ sub reset_refs{
 
   print "$command \n";
   system "$command";
+
+  system "chmod 444 $ref_stdout_file";
+  system "chmod 444 $ref_dsout_file";
 
   print "$ref_stdout_file and $ref_dsout_file reset.\n";
 }
@@ -137,29 +129,33 @@ sub test{
   my $extra_arguments = shift; # e.g. " -un ";
 
   my $stderr_file = "stderr";
-  my $n_failed_tests = 0;
+  my $failed_tests = "";
 
   my $command = "duplicatesearch  -in $input_file  -out $test_dsout_file  2> $stderr_file  > $test_stdout_file ";
   $command .= " -maf 0.08  -accession_max 0.5  -seed 13572467  -t 4 ";
   $command .= $extra_arguments;
-  
-  #print "$command \n";
-  #print `ls -lt *.dsout*`, "\n";
 
   system "$command";
+
+  my $bad_stderr = 0;
+  my $check_genotypes_grep  = `grep 'Successfully completed check_genotypesset' $stderr_file`;
+  $bad_stderr = 1 if(length $check_genotypes_grep == 0);
+  my $stderr_wc_str = `wc -l $stderr_file`;
+  $bad_stderr = 1 if(! ($stderr_wc_str =~ /^\s*6\s/));
+  $failed_tests .= "stderr " if($bad_stderr > 0);
 
   my $diffstr =  `diff $ref_stdout_file $test_stdout_file`;
   my $nnl = () = $diffstr =~ /\n/g;
   #printf("Past test: diff wrt $ref_stdout_file. diff has %d lines.\n", $nnl);
-  $n_failed_tests++ if($nnl != 4);
+  $failed_tests .=  "stdout " if($nnl != 4);
   # print $diffstr;
 
   $diffstr = `diff $ref_dsout_file $test_dsout_file`;
   $nnl = () = $diffstr =~ /\n/g;
   #printf("Past test: diff wrt $ref_dsout_file.  diff has %d lines.\n", $nnl);
-  $n_failed_tests++ if($nnl != 4);
+  $failed_tests .= "dsout " if($nnl != 4);
   # print "[" . $diffstr . "]" . "\n" if($nnl > 0);
-  unlink ($test_stdout_file, $test_dsout_file, $stderr_file) if($n_failed_tests == 0);
-  print "$test_name : ", ($n_failed_tests > 0)? "failed\n" : "passed\n";
-  return $n_failed_tests;
+  unlink ($test_stdout_file, $test_dsout_file) if($failed_tests);
+  print "$test_name : ", ($failed_tests)? "failed\n" : "passed\n";
+  return $failed_tests;
 }
