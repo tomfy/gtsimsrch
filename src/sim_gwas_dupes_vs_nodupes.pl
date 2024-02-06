cbbb8ded8c26d5add0663b38e3dc455457fa404a
tomfy <tly2@cornell.edu>
2024-02-06 09:11:47 -0500

    Script to simulate gwas data, and perform gwas analysis of it. Initial commit.

---
+++ src/sim_gwas_dupes_vs_nodupes.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl -w
+use strict;
+use Getopt::Long;
+use List::Util qw (min max sum);
+
+# simulate accessions with genotypes and phenotypes
+# and add duplicates of accessions in that set
+# with a bit of noise added to the genotypes (but not at present to phenotypes)
+
+# then run plink to get .bed .bim .fam files from  .ped .map
+# run gcta64 to do gwas analysis
+# make first line of .loco.mlma file (gwas output) a comment, and
+# remove lines with 'nan' in them
+
+# my $genotypes_file = undef;
+# my $phenotypes_file = undef;
+# my $alleles_file = undef;
+# my $map_file = undef;
+# my $n_unique = 100;
+# my $n_duplicate = 50;
+
+my $simspec_file = '';
+# the simspec file is something like e.g.:
+# 499 Null1  0.05 0.95 0.0  0
+# 2 Q1  0.05 0.95  0.08  0
+# 499 Null2  0.05 0.95 0.0  0
+# 499 Null3  0.05 0.95 0.0  0
+# 2 Q2  0.05 0.95  0.07  0
+# 499 Null4  0.05 0.95 0.0  0
+
+
+
+my $n_unique_acc = 600;
+my $n_dupe_acc = 400;
+my $error_rate = 0.01;
+
+my $n_reps = 10;
+my $serial_number = 1; # starting serial number
+
+ GetOptions(
+	    'simspec_file=s' => \$simspec_file,
+	    'n_acc|n_uniq=s' => \$n_unique_acc,
+	    'n_dupe=s' => \$n_dupe_acc,
+	    'error_rate=f' => \$error_rate,
+
+	    'serial_number=i' => \$serial_number,
+	    'n_reps=i' => \$n_reps,
+	   );
+
+for(1..$n_reps){
+my $ufile = 'u' . $n_unique_acc . '_' . $serial_number;
+my $udfile = 'u' . $n_unique_acc . 'd' . $n_dupe_acc . '_' . $serial_number;
+
+# simulate; output is in form of  plink 'binary fileset', i.e. .fam, .bim, and .bed files.
+system "plink1.9  --simulate-qt $simspec_file  --simulate-n $n_unique_acc  --out sim_temp ";
+
+# convert to plink 'text fileset' so I can modify.
+system "plink1.9  --bfile sim_temp  --recode  --out sim_temp ";
+
+# make the phenotype file from .fam file
+system "sel_columns '1,2,6' < sim_temp.fam > sim_temp.phen ";
+
+# make the alleles file
+system "sel_columns '2,5,6' < sim_temp.bim  > alleles ";
+
+# make the duplicates, errors added to both unique and duplicate accessions
+my $ped_file = "sim_temp.ped";
+system "perl ~/gtsimsrch/src/rand_dupe_no_dupe.pl  $ped_file sim_temp.phen  alleles  $ufile $udfile $n_unique_acc $n_dupe_acc $error_rate ";
+
+my $umap_file = $ufile . ".map";
+my $map_file = "sim_temp.map";
+system "ln -s $map_file $umap_file";
+my $ubin_file = $ufile . "_bin";
+system "plink1.9  --file $ufile  --out $ubin_file ";
+my $uphen_file = $ufile . ".phen";
+
+system "gcta64  --mlma --bfile $ubin_file  --pheno $uphen_file --out $ufile  --thread-num 2";
+
+my $udmap_file = $udfile . ".map";
+system "ln -s $map_file $udmap_file";
+my $udbin_file = $udfile . "_bin";
+system "plink1.9  --file $udfile  --out $udbin_file ";
+my $udphen_file = $udfile . ".phen";
+
+system "gcta64  --mlma --bfile $udbin_file  --pheno $udphen_file --out $udfile  --thread-num 2";
+
+unlink $umap_file;
+unlink $udmap_file;
+
+$serial_number++;
+}
+
