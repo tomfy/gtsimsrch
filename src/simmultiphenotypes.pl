405d1122a86fa36d9f5edc6109a79a92a0fc89a3
tomfy <tly2@cornell.edu>
2024-03-28 21:05:13 -0400

    now (unless -verbose ) outputs for each accession: id  id  avg_phenotype

--- src/simmultiphenotypes.pl
+++ src/simmultiphenotypes.pl
@@ -2,100 +2,79 @@
 use strict;
 use Getopt::Long;
 use List::Util qw(min max sum shuffle);
-use File::Spec qw(splitpath);
-use File::Basename 'dirname';
-
-use Cwd 'abs_path';
-my $bindir;
-BEGIN {     # this has to go in Begin block so happens at compile time
-  $bindir =
-    dirname( abs_path(__FILE__) ) ; # the directory containing this script
-}
 
 my $binary_fileset = undef;
-# my $uniq_binary_fileset = undef;
-my $n_reps = 1;
-#my $n_causal_snps = 2;
-#my $multiple_chromosomes = 1;
 my $n_phenotypes = 2;
 my $causal_snps_file = undef; # if not specified, choose $n_causal_snps at random to be causal.
-
+my $pheno_out_filename = undef;
+my $verbose = 0;
 GetOptions(
 	   'bin_fileset|fileset_bin=s' => \$binary_fileset,
-	   #'uniq_fileset_bin=s' => \$uniq_binary_fileset,
-	   'n_reps=i' => \$n_reps,
-	   #'n_causal_snps=i' => \$n_causal_snps,
-	   #'multiple_chromosomes!' => \$multiple_chromosomes,
+	   'causal_snps_file=s' => \$causal_snps_file,
 	   'n_phenotypes=i' => \$n_phenotypes,
-	   'causal_snps=s' => \$causal_snps_file,
+	   'pheno_out_filename=s' => \$pheno_out_filename,
+	   'verbose!' => \$verbose,
 	  );
 
 my %accid_outstring = ();
-  my $phenotype_count = 0;
-  # from the full genotype set (with duplicates) simulate data with $n_causal_snps
-  my %accid_phenotypes = ();
-  my $outfile = $binary_fileset;
+my $phenotype_count = 0;
+# from the full genotype set (with duplicates) simulate data with $n_causal_snps
+my %accid_phenotypes = ();
+my $outfile = $binary_fileset;
 
-  my $simu_command;
-  my $causal_variants_filename;
-  if (!defined $causal_snps_file) {
-    die;
+my $causal_variants_filename;
+if (!defined $causal_snps_file) {
+  die;
+} else {
+  $causal_variants_filename = $causal_snps_file . "_just_id";
+  fix_causal_variants_file($causal_snps_file, $causal_variants_filename);
+}
+
+my $phenotype_filename = $outfile . ".pheno";
+my $simu_command = "Simu  --bfile $binary_fileset  --qt  --causal-variants $causal_variants_filename  --out $outfile ";
+my $ref_accid_phenotype = {};
+my $accid_phenotype = {};
+for my $i (1..$n_phenotypes) {
+  my $simu_stdout = `$simu_command`;
+  if ($i == 1) {
+    $ref_accid_phenotype = phenotypes_from_file($phenotype_filename);
+    #print STDERR "size 1: ", scalar keys %$ref_accid_phenotype, "\n";
+    add_phenotypes($ref_accid_phenotype, \%accid_phenotypes);
   } else {
-    $causal_variants_filename = $causal_snps_file . "_x";
-    fix_causal_variants_file($causal_snps_file, $causal_variants_filename);
-  }
-
-  my $phenotype_filename = $outfile . ".pheno";
-
-  $phenotype_count = 0;
-  $simu_command = "Simu  --bfile $binary_fileset  --qt  --causal-variants $causal_variants_filename  --out $outfile ";
-  my $ref_accid_phenotype = {};
-  my $accid_phenotype = {};
-  for my $i (1..$n_phenotypes) {
-    system "$simu_command";
-    if($i == 1){
-      $ref_accid_phenotype = phenotypes_from_file($phenotype_filename);
-      print STDERR "size 1: ", scalar keys %$ref_accid_phenotype, "\n";
-      add_phenotypes($ref_accid_phenotype, \%accid_phenotypes);
-    }else{
-      $accid_phenotype = phenotypes_from_file($phenotype_filename);
-      print STDERR "size 2: ", scalar keys %$accid_phenotype, "\n";
-      my $the_slope = slope($ref_accid_phenotype, $accid_phenotype);
-      print STDERR "slope: $the_slope\n";
-      if($the_slope < 0){
-	flip_signs($accid_phenotype);
-      }
-      add_phenotypes($accid_phenotype, \%accid_phenotypes);
-      $the_slope = slope($ref_accid_phenotype, $accid_phenotype);
-      print STDERR "slope: $the_slope\n";
+    $accid_phenotype = phenotypes_from_file($phenotype_filename);
+    #print STDERR "size 2: ", scalar keys %$accid_phenotype, "\n";
+    my $the_slope = slope($ref_accid_phenotype, $accid_phenotype);
+    print STDERR "slope: before: $the_slope;   ";
+    if ($the_slope < 0) {
+      flip_signs($accid_phenotype);
     }
-  
-    
-    $phenotype_count++;
+    add_phenotypes($accid_phenotype, \%accid_phenotypes);
+    $the_slope = slope($ref_accid_phenotype, $accid_phenotype);
+    print STDERR "after: $the_slope\n";
   }
+  $phenotype_count++;
+}
+die if($phenotype_count !=  $n_phenotypes);
+while (my ($id, $phs) = each %accid_phenotypes) {
+  die if(scalar @$phs !=  $n_phenotypes);
+  $accid_outstring{$id} = "$id  $id " . sum(@$phs)/$n_phenotypes;
+  if ($verbose) {
+    $accid_outstring{$id} .= "   $n_phenotypes  " . join(" ", @$phs);
+  }
+}
 
-  while(my ($id, $phs) = each %accid_phenotypes){
-    if(!exists $accid_outstring{$id}){
-      $accid_outstring{$id} = "$id   $n_phenotypes  ";
-    }
-    $accid_outstring{$id} .= sum(@$phs)/$n_phenotypes . "  " . join(" ", @$phs) . "  ";
-  }
-#}
-
-  my @sorted_ids = sort keys %accid_outstring;
-  open my $fhout, ">", $n_phenotypes . "_phenotypes_" . $n_reps . "_reps";
-  for my $anid (@sorted_ids){
-    print $fhout  $accid_outstring{$anid}, "\n";
-  }
-  close $fhout;
+my @sorted_ids = sort keys %accid_outstring;
+open my $fhout, ">", $n_phenotypes . "_phenotypes";
+for my $anid (@sorted_ids) {
+  print $fhout  $accid_outstring{$anid}, "\n";
+}
+close $fhout;
 
 
 sub add_phenotypes{
   my $accid_ph = shift;
   my $accid_phenos = shift;
   while (my ($id, $pheno) = each %$accid_ph) {
-    #next if(/^\s*FID/);
-   # my ($fid, $iid, $pheno) = split(" ", $_);
     $accid_phenos->{$id} = [] if(!exists $accid_phenos->{$id});
     push @{ $accid_phenos->{$id} }, $pheno;
   }
@@ -139,7 +118,7 @@ sub slope{
   my $avg_phe2 = sum(values %$aph2)/$size;
   my $numerator = 0;
   my $denominator = 0;
-  while(my($id, $aphe1) = each %$aph1){
+  while (my($id, $aphe1) = each %$aph1) {
     my $aphe2 = $aph2->{$id};
     $numerator += ($aphe1 - $avg_phe1)*($aphe2 - $avg_phe2);
     $denominator += ($aphe1 - $avg_phe1)**2;
