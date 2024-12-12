fbb9959d5d8fb2746d7150fe5c39e44cfefbb9d4
tomfy <tly2@cornell.edu>
2024-12-12 17:08:01 -0500

    print number of heterozygs, and also numbers of -1, and +1 gts: e.g. ... 1345 (680, 665) ...

--- src/dosage_matrix_info.pl
+++ src/dosage_matrix_info.pl
@@ -21,7 +21,7 @@ while (<>) {
     # }
     $n_marker_ids = scalar @cols - 1;
     print "# n marker ids: $n_marker_ids\n";
-  }elsif(/^CHROMOSOME/) {
+  }elsif(/^CHROMOSOME/) { # ignore this line
     
   } else {
     s/^\s*(\S+)//; # delete first field (accession id)
@@ -29,10 +29,12 @@ while (<>) {
  #  while (/\s+\S/g) { $marker_count++ } # slow compared to using tr as in following 4 lines.
     my $zero_count = $_ =~ tr/0/0/;
     my $one_count = $_ =~ tr/1/1/;
-    my $neg_count = $_ =~ tr/-/-/;
+    my $minus_count = $_ =~ tr/-/-/;
+    my $plus_count = $_ =~ tr/+/+/;
     my $two_count = $_ =~ tr/2/2/;
     my $X_count = $_ =~ tr/X/X/;
-    print STDERR "$acc_id   $zero_count  $one_count  $two_count  $X_count   $neg_count\n";
+    #my $acc_heterozygosity = $one_count/($zero_count + $one_count + $two_count);
+    # print STDERR "$acc_id   $zero_count  $one_count ($minus_count, $plus_count)  $two_count  $X_count  $acc_heterozygosity\n";
     my $marker_count =  $zero_count+$one_count+$two_count+$X_count;
     die "# Marker count discrepancy: n_marker_ids $n_marker_ids  n_markers: $marker_count\n" if($marker_count != $n_marker_ids);
     $cume_zero_count += $zero_count;
@@ -45,7 +47,8 @@ while (<>) {
     my $alt_allele_freq = ($ok_count > 0)? ($one_count + 2*$two_count)/(2*$ok_count) : -1;
     my $maf = ($alt_allele_freq <= 0.5)? $alt_allele_freq : 1.0 - $alt_allele_freq;
     my $heterozygosity = ($ok_count > 0)? $one_count/$ok_count : -1;
-    printf(STDOUT "%6ld %36s   %7ld %7ld %7ld %7ld   %7ld   %8.4f    %8.4f\n", $n_accessions, $acc_id, $zero_count, $one_count, $two_count, $X_count, $marker_count, $maf, $heterozygosity);
+    printf(STDOUT "%5ld %30s   %6ld %6ld (%5d,%5d) %5ld %7ld   %7ld   %8.4f    %8.4f\n",
+	   $n_accessions, $acc_id, $zero_count, $one_count, $minus_count, $plus_count, $two_count, $X_count, $marker_count, $maf, $heterozygosity);
    # print STDOUT "#           ", $one_count + 2*$two_count, "  ", $ok_count, "  ", $alt_allele_freq, "\n"
   }
 }
