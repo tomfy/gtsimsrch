c267cf031ffb7d545d5d05d7f8bd47b857defacc
tomfy <tly2@cornell.edu>
2025-12-09 15:00:05 -0500

    split on tabs get acc ids from cols 1, 3, 4

--- other/triples_3_cases.pl
+++ other/triples_3_cases.pl
@@ -2,13 +2,13 @@
 use strict;
 use Graph::Directed;
 
-# construct DAG from pedigree_test output
+# construct DAG from find_parents output
 # output ids of triples with selected relationship:
 # 1: accession, parent1, parent2,
-# 2: accession, progeny1, progeny2
-# 3: accession, parent, progeny
+# 2: accession, offspring1, offspring2
+# 3: accession, parent, offspring
 
-# typical usage:  triple_3_cases.pl  2 < simsrch.out > aoo.out
+# typical usage:  triple_3_cases.pl  2 < find_parents.out > aoo_pedigrees
 
 my $case = shift // 1;
 
@@ -17,20 +17,20 @@ my $case = shift // 1;
 my $g = Graph::Directed->new;
 my %ids = ();	   # just stores all the ids of the nodes in the graph
 my $edge_count = 0;
-while (<>) { # read pedigree_test output file and add edges to the graph
+while (<>) { # read find_parents output file and add edges to the graph
   next if(/^\s*#/);		# skip comment lines
   my @cols = split(" ", $_);
-  my ($aid, $pid1, $pid2) = @cols[0,3,4]; # @cols[0,2,3];
+  my ($aid, $pid1, $pid2) = @cols[0,2,3]; # @cols[0,2,3];
   next if($aid eq 'NA');
   $ids{$aid} = 1;
   if ($pid1 ne 'NA') {
     $ids{$pid1} = 1;
-    $g->add_edge($pid1, $aid);
+    $g->add_edge($pid1, $aid); # edge goes from parent to offspring
     $edge_count++;
   }
   if ($pid2 ne 'NA') {
     $ids{$pid2} = 1;
-    $g->add_edge($pid2, $aid);
+    $g->add_edge($pid2, $aid); # edge goes from parent to offspring
     $edge_count++;
   }
 }
@@ -45,18 +45,18 @@ print STDERR "# the graph is ", ($is_DAG)? "a DAG" : "NOT a DAG", "\n";
 
 for my $anid (keys %ids){
     my $in_degree = $g->in_degree($anid);
-    print STDERR "# $anid has in_degree of $in_degree\n" if($in_degree > 2);
+    print STDERR "$anid has in_degree of $in_degree\n" if(1  or $in_degree > 2);
     my $out_degree = $g->out_degree($anid);
     if($case == 1){ 
 	if($in_degree == 2){
-	    my $str = "$anid ";
+	    my $str = "$anid";
 	    my @edges_to = $g->edges_to($anid); # list of edges to the node;
 	    # edge is array ref of 2 node ids; directed edge goes from zeroth to first element
 	    for my $e (@edges_to){
-		$str .= " " . $e->[0];
+		$str .= "\t" . $e->[0];
 		# $str .= join(" ", @$e) . " ";
 	    }
-	    print "$str \n";
+	    print "$str\n";
 	}else{
 	    # too few parents known, can't do case 1
 	}
